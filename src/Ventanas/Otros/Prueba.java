2d3fb42de73d1a33ccc13b299878978cff87209a
Cristopher B. <codepher@gmail.com>
2024-05-05 17:50:33 -0700

    ..

--- src/Ventanas/Otros/Prueba.java
+++ src/Ventanas/Otros/Prueba.java
@@ -1,11 +1,18 @@
 package Ventanas.Otros;
 
-import java.util.Random;
+import org.tensorflow.SavedModelBundle;
+import org.tensorflow.Session;
 
 public class Prueba {
 
     public static void main(String[] args) {
-        
+        String rutaModeloH5 = "C:\\Users\\Cristopher\\Downloads\\model.h5";
+        try (SavedModelBundle model = SavedModelBundle.load(rutaModeloH5, "serve")) {
+            Session session = model.session();
+            System.out.println(model);
+            System.out.println(session);
+        } catch (Exception e) {
+        }
     }
 
 }
